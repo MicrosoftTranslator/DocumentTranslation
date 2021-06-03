ee290f1495c93c25617391df3183153babf69d68
Chris Wendt <chris.wendt@outlook.com>
2021-06-03 13:15:45 -0700

    Strings to resource file. Tooltips for relevant items in the UI.

--- DocumentTranslation.GUI/Properties/Resources.Designer.cs
+++ DocumentTranslation.GUI/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace DocumentTranslation.GUI.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace DocumentTranslation.GUI.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DocumentTranslation.GUI.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,399 @@ namespace DocumentTranslation.GUI.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Add.
+        /// </summary>
+        public static string button_Add {
+            get {
+                return ResourceManager.GetString("button_Add", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Browse.
+        /// </summary>
+        public static string button_Browse {
+            get {
+                return ResourceManager.GetString("button_Browse", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cancel.
+        /// </summary>
+        public static string button_Cancel {
+            get {
+                return ResourceManager.GetString("button_Cancel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Clear.
+        /// </summary>
+        public static string button_Clear {
+            get {
+                return ResourceManager.GetString("button_Clear", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Delete.
+        /// </summary>
+        public static string button_Delete {
+            get {
+                return ResourceManager.GetString("button_Delete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Save.
+        /// </summary>
+        public static string button_Save {
+            get {
+                return ResourceManager.GetString("button_Save", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Select.
+        /// </summary>
+        public static string button_Select {
+            get {
+                return ResourceManager.GetString("button_Select", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Test.
+        /// </summary>
+        public static string button_Test {
+            get {
+                return ResourceManager.GetString("button_Test", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Translate.
+        /// </summary>
+        public static string button_Translate {
+            get {
+                return ResourceManager.GetString("button_Translate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Translate Documents.
+        /// </summary>
+        public static string button_TranslateDocuments {
+            get {
+                return ResourceManager.GetString("button_TranslateDocuments", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Azure Region:.
+        /// </summary>
+        public static string label_AzureRegion {
+            get {
+                return ResourceManager.GetString("label_AzureRegion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Category:.
+        /// </summary>
+        public static string label_Category {
+            get {
+                return ResourceManager.GetString("label_Category", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Documents to translate.
+        /// </summary>
+        public static string label_DocumentsToTranslate {
+            get {
+                return ResourceManager.GetString("label_DocumentsToTranslate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to From:.
+        /// </summary>
+        public static string label_From {
+            get {
+                return ResourceManager.GetString("label_From", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Glossary to use (optional):.
+        /// </summary>
+        public static string label_Glossary {
+            get {
+                return ResourceManager.GetString("label_Glossary", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Input:.
+        /// </summary>
+        public static string label_Input {
+            get {
+                return ResourceManager.GetString("label_Input", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Resource Name:.
+        /// </summary>
+        public static string label_ResourceName {
+            get {
+                return ResourceManager.GetString("label_ResourceName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Show experimental languages:.
+        /// </summary>
+        public static string label_ShowExperimental {
+            get {
+                return ResourceManager.GetString("label_ShowExperimental", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Storage Connection String:.
+        /// </summary>
+        public static string label_StorageConnectionString {
+            get {
+                return ResourceManager.GetString("label_StorageConnectionString", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Subscription Key:.
+        /// </summary>
+        public static string label_SubscriptionKey {
+            get {
+                return ResourceManager.GetString("label_SubscriptionKey", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Target folder:.
+        /// </summary>
+        public static string label_TargetFolder {
+            get {
+                return ResourceManager.GetString("label_TargetFolder", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to To:.
+        /// </summary>
+        public static string label_To {
+            get {
+                return ResourceManager.GetString("label_To", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Translation:.
+        /// </summary>
+        public static string label_Translation {
+            get {
+                return ResourceManager.GetString("label_Translation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Categories saved.
+        /// </summary>
+        public static string msg_CategoriesSaved {
+            get {
+                return ResourceManager.GetString("msg_CategoriesSaved", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Settings saved.
+        /// </summary>
+        public static string msg_SettingsSaved {
+            get {
+                return ResourceManager.GetString("msg_SettingsSaved", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Test passed.
+        /// </summary>
+        public static string msg_TestPassed {
+            get {
+                return ResourceManager.GetString("msg_TestPassed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Authentication.
+        /// </summary>
+        public static string tabTitle_Authentication {
+            get {
+                return ResourceManager.GetString("tabTitle_Authentication", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Categories.
+        /// </summary>
+        public static string tabTitle_Categories {
+            get {
+                return ResourceManager.GetString("tabTitle_Categories", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Settings.
+        /// </summary>
+        public static string tabTitle_Settings {
+            get {
+                return ResourceManager.GetString("tabTitle_Settings", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Translate Documents.
+        /// </summary>
+        public static string tabTitle_TranslateDocuments {
+            get {
+                return ResourceManager.GetString("tabTitle_TranslateDocuments", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Translate Text.
+        /// </summary>
+        public static string tabTitle_TranslateText {
+            get {
+                return ResourceManager.GetString("tabTitle_TranslateText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to This list of category names provides easy access to a friendly name for custom translation systems you have built with Custom Translator. More information at http://customtranslator.ai. No need to use this unless you have built a custom translation system. .
+        /// </summary>
+        public static string text_Categories {
+            get {
+                return ResourceManager.GetString("text_Categories", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Obtain the subscription key, the Azure region, the resource name and the storage connection string from the Azure portal or from your organization&apos;s Azure administrator..
+        /// </summary>
+        public static string text_SettingsInstructions {
+            get {
+                return ResourceManager.GetString("text_SettingsInstructions", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Document Translation.
+        /// </summary>
+        public static string title_WindowTitle {
+            get {
+                return ResourceManager.GetString("title_WindowTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The Azure region your Translator resource resides in. .
+        /// </summary>
+        public static string tooltip_AzureRegion {
+            get {
+                return ResourceManager.GetString("tooltip_AzureRegion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Use the &quot;Settings&quot; tab, &quot;Categories&quot; to define a set of Custom Translator categories you want to use. Leave the selection clear if you are not using Custom Translator..
+        /// </summary>
+        public static string tooltip_Category {
+            get {
+                return ResourceManager.GetString("tooltip_Category", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Clears the selection of a category. Does not remove the category fron your set of available categories..
+        /// </summary>
+        public static string tooltip_CategoryClear {
+            get {
+                return ResourceManager.GetString("tooltip_CategoryClear", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The name of the Translator resource in your Azure subscription..
+        /// </summary>
+        public static string tooltip_ResourceName {
+            get {
+                return ResourceManager.GetString("tooltip_ResourceName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Save the above settings on your device..
+        /// </summary>
+        public static string tooltip_SaveSettings {
+            get {
+                return ResourceManager.GetString("tooltip_SaveSettings", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Whether to show the languages the Translator service considers experimental. Experimental languages may not be available in both directions and the translation can fail. Leave unchecked unless you need to experiment with not fully supported languages..
+        /// </summary>
+        public static string tooltip_ShowExperimental {
+            get {
+                return ResourceManager.GetString("tooltip_ShowExperimental", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Copy the &quot;Connection String&quot; from the Properties page of the Storage resource in your Azure subscription..
+        /// </summary>
+        public static string tooltip_StorageConnectionString {
+            get {
+                return ResourceManager.GetString("tooltip_StorageConnectionString", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Copy the &quot;Key 1&quot; or &quot;Key 2&quot; of the Translator resource in your Azure subscription..
+        /// </summary>
+        public static string tooltip_SubscriptionKey {
+            get {
+                return ResourceManager.GetString("tooltip_SubscriptionKey", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Invoke a test of teh above settings. Will show a pass or fail result. .
+        /// </summary>
+        public static string tooltip_TestSettings {
+            get {
+                return ResourceManager.GetString("tooltip_TestSettings", resourceCulture);
             }
         }
     }
